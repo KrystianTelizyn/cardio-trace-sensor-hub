56a04bf6ee43c7874e1fc8773901f9da4e323203
KrystianTelizyn <krysian.telizyn@gmail.com>
2026-05-11 12:33:19 +0200

    refactor: imports cleaned up

--- src/app/pipeline_steps/__init__.py
+++ src/app/pipeline_steps/__init__.py
@@ -1,23 +1,13 @@
 from app.pipeline_steps.backend_identification import BackendIdentificationStep
-from app.pipeline_steps.base import (
-    BACKEND_IDENTIFICATION_STEP,
-    DEVICE_DISCOVERY_STEP,
-    PipelineStep,
-    SAVE_RECORD_STEP,
-    TENANT_IDENTIFICATION_STEP,
-)
+from app.pipeline_steps.base import PipelineStep
 from app.pipeline_steps.device_discovery import DeviceDiscoveryStep
 from app.pipeline_steps.save_record import SaveRecordStep
 from app.pipeline_steps.tenant_identification import TenantIdentificationStep
 
 __all__ = [
-    "BACKEND_IDENTIFICATION_STEP",
     "BackendIdentificationStep",
-    "DEVICE_DISCOVERY_STEP",
     "DeviceDiscoveryStep",
     "PipelineStep",
-    "SAVE_RECORD_STEP",
     "SaveRecordStep",
-    "TENANT_IDENTIFICATION_STEP",
     "TenantIdentificationStep",
 ]
